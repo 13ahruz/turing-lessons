253d1550f13c1e5873e2957c117ab4191c44cdf7
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-29 20:27:19 +0400

    Some changes

---
+++ turing-lessons/src/edu/lesson9/MethodsApp.java
@@ -0,0 +1,21 @@
+package edu.lesson9;
+
+import java.util.Random;
+
+public class MethodsApp {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = new Random().nextInt(100);
+        }
+
+        PrintArray(arr);
+    }
+
+    public static void PrintArray(int[] array) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i + " - " + array[i]);
+        }
+    }
+}
