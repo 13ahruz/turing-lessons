9ca833eda7664c896c2b35a9db85b4d8bb7e4651
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-29 19:41:06 +0400

    Some changes

--- turing-lessons/src/edu/lesson9/FunctionsApp.java
+++ turing-lessons/src/edu/lesson9/FunctionsApp.java
@@ -3,33 +3,33 @@ package edu.lesson9;
 import java.util.Scanner;
 
 public class FunctionsApp {
-        public static void main(String[] args) {
-            System.out.print("Enter tile of square: ");
-            int tile = new Scanner(System.in).nextInt();
+    public static void main(String[] args) {
+        System.out.print("Enter tile of square: ");
+        int tile = new Scanner(System.in).nextInt();
 
+        System.out.println("Enter radius of circle: ");
+        int radius = new Scanner(System.in).nextInt();
 
-            System.out.println("Enter radius of circle: ");
-            int radius = new Scanner(System.in).nextInt();
-
-            System.out.println(AreaOfSquare(tile));
-            System.out.println(PerimeterOfSquare(tile));
-        }
-
-        public static int AreaOfSquare (int tile){
-            return tile*tile;
-        }
-
-        public static int PerimeterOfSquare (int tile){
-            return tile * 4;
-        }
-
-        public static double AreaOfCircle (int radius){
-            return Math.PI * radius * radius;
-        }
-
-        public static double LengthOfCircle (int radius){
-            return 2 * Math.PI * radius;
-        }
-
+        System.out.println("Area of square is: " + AreaOfSquare(tile));
+        System.out.println("Perimeter of square is: " + PerimeterOfSquare(tile));
+        System.out.println("Area of circle is: " + AreaOfCircle(radius));
+        System.out.println("Perimeter of circle is: " + PerimeterOfSquare(radius));
     }
+
+    public static int AreaOfSquare(int tile) {
+        return tile * tile;
+    }
+
+    public static int PerimeterOfSquare(int tile) {
+        return tile * 4;
+    }
+
+    public static double AreaOfCircle(int radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static double LengthOfCircle(int radius) {
+        return 2 * Math.PI * radius;
+    }
+}
 
