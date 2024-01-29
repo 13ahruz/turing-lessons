8716feb5322f4dcff64e7531ab5002ad063cc500
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-29 19:37:33 +0400

    Some changes

--- turing-lessons/src/edu/lesson9/FunctionsApp.java
+++ turing-lessons/src/edu/lesson9/FunctionsApp.java
@@ -4,19 +4,31 @@ import java.util.Scanner;
 
 public class FunctionsApp {
         public static void main(String[] args) {
-
+            System.out.print("Enter tile of square: ");
             int tile = new Scanner(System.in).nextInt();
 
-            System.out.println(Area(tile));
-            System.out.println(Perimeter(tile));
+
+            System.out.println("Enter radius of circle: ");
+            int radius = new Scanner(System.in).nextInt();
+
+            System.out.println(AreaOfSquare(tile));
+            System.out.println(PerimeterOfSquare(tile));
         }
 
-        public static int Area (int tile){
+        public static int AreaOfSquare (int tile){
             return tile*tile;
         }
 
-        public static int Perimeter (int tile){
+        public static int PerimeterOfSquare (int tile){
             return tile * 4;
+        }
+
+        public static double AreaOfCircle (int radius){
+            return Math.PI * radius * radius;
+        }
+
+        public static double LengthOfCircle (int radius){
+            return 2 * Math.PI * radius;
         }
 
     }
