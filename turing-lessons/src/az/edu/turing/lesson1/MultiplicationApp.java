f800e4a86c9d29381c904787ebb7fa2c6cd093dd
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-01-31 20:07:51 +0400

    New Apps Loaded

---
+++ turing-lessons/src/az/edu/turing/lesson1/MultiplicationApp.java
@@ -0,0 +1,9 @@
+package az.edu.turing.lesson1;
+
+public class MultiplicationApp {
+    public static void main(String[] args){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(2 * i);
+        }
+    }
+}
---
+++ turing-lessons/src/az/edu/turing/lesson5/SumSquareApp.java
@@ -0,0 +1,11 @@
+package az.edu.turing.lesson5;
+
+import java.util.Scanner;
+
+public class SumSquareApp {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+
+        System.out.println((num / 10 + num % 10) * (num / 10 + num % 10));
+    }
+}
