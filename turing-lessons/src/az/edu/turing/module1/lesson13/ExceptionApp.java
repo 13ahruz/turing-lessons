6099bb20b41cf304dd6f1ec164ac36dce681b8dc
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-03-11 20:05:50 +0400

    Some apps committed

---
+++ turing-lessons/src/az/edu/turing/module1/lesson13/ExceptionApp.java
@@ -0,0 +1,15 @@
+package az.edu.turing.module1.lesson13;
+
+import com.sun.source.tree.TryTree;
+
+import java.util.Scanner;
+
+public class ExceptionApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input ur input: ");
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
