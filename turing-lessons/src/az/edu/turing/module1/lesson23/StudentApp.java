bebdecbef2f1fa90f077cda9186dbe3775b12bb5
Bahruz Shabili <bahruz.dsh@code.edu.az>
2024-04-22 19:34:57 +0400

    StudentApp committed

---
+++ turing-lessons/src/az/edu/turing/module1/lesson23/StudentApp.java
@@ -0,0 +1,15 @@
+package az.edu.turing.module1.lesson23;
+
+import java.util.ArrayList;
+
+public class StudentApp {
+    public static ArrayList <Student> students;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void addStudent (Student st){
+        students.add(st);
+    }
+}
